e8a6657854bddb52db51e6dc59178d2e4ea50349
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-19 23:06:07 +0200

    4 pr solved

---
+++ seminar_1_DZ/4_problem/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Write("Введите число больше 1 > ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int evenCount = 2;
+
+if (numberN > 0)
+{
+    while (evenCount <= numberN)
+    {
+        Console.Write(evenCount + ", ");
+        evenCount = evenCount + 2;
+    }
+}
+else
+Console.WriteLine("Введите число больше 1");
