ff106d9ee96dc728f2fb811aaa40de8f2b294616
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-19 17:50:45 +0200

    second problem solved

--- seminar_1/problem_2/Program.cs
+++ seminar_1/problem_2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа 
+// и проверяет, является ли второе число квадратом первого.
+// a = 5; b = 25 -> да a = 2 b = 10 -> нет a = 9; b = -3 -> нет a = -3 b = 9 -> да
+
+
+Console.Write("Введите первое число > ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число > ");
+int secondNumber = Convert.ToInt32(Console.ReadLine()); 
+
+if (secondNumber == firstNumber * firstNumber)
+{
+    Console.WriteLine($"Число {secondNumber} является квадратом {firstNumber}");
+}
+else 
+{
+    Console.WriteLine($"Число {secondNumber} не является квадратом {firstNumber}");
+}
