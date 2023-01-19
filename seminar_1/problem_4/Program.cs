93aa041f80b2f884b9c1e412920d4f08f2437d92
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-19 19:18:43 +0200

    4 pr solved

---
+++ seminar_1/problem_4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу вычисления модуля числа. 2 -> 2 -3 -> 3 -7 -> 7
+
+Console.Write("Введите число > ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    number = -number;
+}
+Console.WriteLine(number);
