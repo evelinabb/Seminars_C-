2c39cd41ec4c18853a3864868742ae376cadbba7
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-24 23:09:40 +0200

    5 pr solved

---
+++ seminar_2/problem_5/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Напишите программу, которая выводит случайное число, которое введет пользователь и показывает наибольшую цифру числа.
+*/
+
+int InputInt(string message)
+{
+    Console.Write(message + " > ");                // Вывод сообщения пользователю
+    string inputValue = Console.ReadLine();        // Ввод строки пользователем
+    int result = Convert.ToInt32(inputValue);      // Преобразование в целое введенной строки
+    return result;                                 // возврат результата
+}
+
+
+int Max(int num)
+{
+    int max = 0;
+    while (num > 0)
+    {
+        int temp = num % 10;
+        if (temp > max)
+        {
+            max = temp;
+        }
+        num = num / 10;
+    }
+    return max;
+}
+
+int number = InputInt("Введите число");
+int maxNumber = Max(number);
+Console.WriteLine($"Максимальная цифра в числе {number} : {maxNumber}");
+
