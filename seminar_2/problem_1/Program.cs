52c519e766318af60b9833ff1106a1e87b065443
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-24 16:19:11 +0200

    1 pr solved

---
+++ seminar_2/problem_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на 
+// выходе показывает последнюю цифру этого числа.
+
+// Это функция, которую можно вызвать. Ввод числа с консоли
+
+int InputInt (string message)
+{
+    Console.Write(message + " > ");                // Вывод сообщения пользователю
+    string inputValue = Console.ReadLine();        // Ввод строки пользователем
+    int result = Convert.ToInt32(inputValue);      // Преобразование в целое введенной строки
+    return result;                                 // возврат результата
+}
+
+// Проверка на 3-хзначность
+bool ValidateNumberRank3 (int number)
+{
+    if (number > 99 && number < 1000)               // Проверка на трехзначность
+    {
+        return true;
+    }
+    System.Console.WriteLine("Число не трехзначное.");  // Пользователь ввел не трехзначное число
+    return false;
+}
+
+int number = InputInt("Ввевите трехзначное число"); // Вызов функции ввода
+
+if (ValidateNumberRank3(number))                    // Проверяем на трехзначность
+{
+    int lastRank = number % 10;                      // последняя цифра
+    System.Console.WriteLine($"Последняя цифра числа {number} равна {lastRank}");
+}
