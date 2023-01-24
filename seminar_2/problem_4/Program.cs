f0e6fb7e60b6d8b5a93bf8984b5b11e6424ebd04
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-24 22:48:39 +0200

    4 pr solved

---
+++ seminar_2/problem_4/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+(усложняем: на вход принимаем три числа, и проверяем, кратно ли первое двум вторым)
+14 -> нет 
+46 -> нет 
+161 -> да */
+
+int InputInt(string message)
+{
+    Console.Write($"Введите {message} число > ");                // Вывод сообщения пользователю
+    string inputValue = Console.ReadLine();        // Ввод строки пользователем
+    int result = Convert.ToInt32(inputValue);      // Преобразование в целое введенной строки
+    return result;                                 // возврат результата
+}
+
+
+bool CheckNumber(int first, int second, int third)
+{
+    if (first % second == 0 && first % third == 0)
+    {
+        return true;
+    }
+    else return false;
+}
+
+int firstNumber = InputInt("первое");
+int secondNumber = InputInt("второе");
+int thirdNumber = InputInt("третье");
+
+bool check = CheckNumber(firstNumber, secondNumber, thirdNumber);
+Console.WriteLine(check);
