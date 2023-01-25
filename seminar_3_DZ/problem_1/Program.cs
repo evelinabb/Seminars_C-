a1a5ce3e25af098c5b7e40ce3723e297f60fad22
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-25 23:05:52 +0200

    1 pr solved

--- seminar_3_DZ/problem_1/Program.cs
+++ seminar_3_DZ/problem_1/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Не используйте строки
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+int PromptInt(string message)
+{
+    Console.Write($"{message} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+bool FindFirstAndLastDigit (int number)
+{
+    int firstDigit = (number/10000) % 10;
+    int lastDigit = number % 10;
+    if (firstDigit == lastDigit)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool FindSecondAndPenultimateDigit (int number)
+{
+    int secondDigit = (number/1000) % 10;
+    int penultimateDigit = (number/10) % 10;
+    if (secondDigit == penultimateDigit)
+    {
+        return true;
+    }
+    return false;
+}
+
+int N = PromptInt("Введите число");
+if (FindFirstAndLastDigit(N))
+{
+    if (FindSecondAndPenultimateDigit(N))
+    {
+        Console.WriteLine("Число является полиндромом");
+    }
+}
+else 
+{
+    Console.WriteLine("Число не является полиндромом");
+}
