1b6b28239cbaf50201d149aac3c762b26f3da0e0
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-27 16:37:37 +0200

    added pr w/ *

---
+++ seminar_3/problem_6_seminar_so_zvezdochkoi/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 6*: Напишите программу, которая выводит первые 2 заданные элемента следующей последовательности: 
+// -> 12
+// 1 2 2 3 3 3 4 4 4 4 5 5
+
+
+int PromptInt(string message)
+{
+    Console.Write($"{message} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    collection[index] = 1;
+    while (index < length)
+    {
+        while (collection[index] < collection[index] + 1)
+        {
+            System.Console.Write(collection[index] + ", ");
+        }
+        index++;
+        collection[index]++;
+    }
+}
+
+int number = PromptInt("Введите число");
+int[] array = new int[number];
+
+PrintArray(array);
+
+
+
+
+
