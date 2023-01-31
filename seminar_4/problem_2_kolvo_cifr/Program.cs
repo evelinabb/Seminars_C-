ad999a6a9c38dd8de32e9afdc76675f445c98897
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-31 03:10:11 +0200

    2 pr solved

---
+++ seminar_4/problem_2_kolvo_cifr/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+// 84973 -> 5
+
+int PromptInt(string message)
+{
+    Console.Write($"{message} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+int CountRank(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+
+
+int num = PromptInt("Vvedite cislo");
+System.Console.WriteLine(CountRank(num));
