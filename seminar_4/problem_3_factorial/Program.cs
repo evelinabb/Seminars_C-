ac0003688a1932342f815f11b773ae88a2c06662
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-01-31 03:10:36 +0200

    3 pr solved

---
+++ seminar_4/problem_3_factorial/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+int Prompt(string message)
+{
+    Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int Factorial(int number)
+{
+    
+    // int fakt = 1;                                    //toze pravilno
+    // for (int i = 1; i <= number; i++)
+    // {
+    //     fakt *= i;
+    // }
+    // return fakt;
+
+    int fact = 1;
+    while(number >0)
+    {
+        fact *= number;
+        number -=1;
+    }
+    return fact;
+}
+
+
+int number = Prompt("Vvedite cislo");
+Console.WriteLine($"Factorial cisla {number} = {Factorial(number)}");
+
