5b8b5aa6fb5247c363e623443f02f8ebfd070268
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-02-08 17:33:43 +0200

    solved problems

--- seminar_6_DZ/problem_1/Program.cs
+++ seminar_6_DZ/problem_1/Program.cs
@@ -3,5 +3,51 @@
 // 1, -7, 567, 89, 223-> 3
 
 
+int[] CreateArray(string msg)
+{
+    Console.Write(msg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[number];
+    return array;
+}
+
+int[] FillArray(int[] array, string msg)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(msg);
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array, string msg)
+{
+    Console.Write(msg);
+    foreach (int item in array)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
+int PositiveNumCount(int[] array)
+{
+    int count = 0;
+    foreach (int item in array)
+    {
+        if (item > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] userArray = CreateArray("Vvedite dlinu massiva > ");
+FillArray(userArray, "Vvedite cislo > ");
+PrintArray(userArray, "Vash massiv: ");
+Console.WriteLine($"Kolicestvo polozitelnych cisel v massive: {PositiveNumCount(userArray)}");
+
 
 
--- seminar_6_DZ/problem_2/Program.cs
+++ seminar_6_DZ/problem_2/Program.cs
@@ -1,5 +1,34 @@
-﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+int InputInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[] IntersectionArray(double num1, double num2, double num3, double num4)
+{
+    double[] array = new double[2];
+    array[0] = (num3 - num1) / (num2 - num4);
+    array[1] = num2 * array[0] + num1;
+    return array;
+}
+
+void PrintArray(double[] array, string message)
+{
+    Console.Write(message);
+    Console.Write("(" + array[0] + ", ");
+    Console.Write(array[1] + ")");
+
+}
+
+int numberb1 = InputInt("Vvedite b1 > ");
+int numberk1 = InputInt("Vvedite k1 > ");
+int numberb2 = InputInt("Vvedite b2 > ");
+int numberk2 = InputInt("Vvedite k2 > ");
+double[] coordinates = IntersectionArray(numberb1, numberk1, numberb2, numberk2);
+PrintArray(coordinates, "Tocka peresecenia imeet koordinaty:");
 
 
