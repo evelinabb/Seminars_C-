372fe8ac70ad9c8126ccb7336b8f35378cae52d4
Evelina Brinziuc <evelinabrinziuc@gmail.com>
2023-02-17 17:48:07 +0200

    solved problems

---
+++ seminar_9_DZ/problem_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные 
+// натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int Prompt(string msg)
+{
+    Console.Write($"{msg} -> ");
+    return int.Parse(Console.ReadLine());
+}
+
+void EvenNumbers(int n, int m)
+{
+    n += n % 2;
+    if (n > m)
+    {
+        return;
+    }
+    Console.Write($"{n}; ");
+    EvenNumbers(n + 2, m);
+}
+
+int numberM = Prompt("Vvedite cislo M");
+int numberN = Prompt("Vvedite cislo N");
+EvenNumbers(numberM, numberN);
+
+
---
+++ seminar_9_DZ/problem_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Prompt(string msg)
+{
+    Console.Write($"{msg} -> ");
+    return int.Parse(Console.ReadLine());
+}
+
+int SumNumbers(int first, int last)
+{
+    if (first>last)
+    {
+        return 0;
+    }
+    return first + SumNumbers(first+1, last);
+}
+
+
+int numberM = Prompt("Vvedite pervoe cislo");
+int numberN = Prompt("Vvedite poslednee cislo");
+int result = SumNumbers(numberM, numberN);
+Console.WriteLine($"Summa cisel ot {numberM} do {numberN} ravna {result}");
+
